11c777779b9888edc98e5277823952cd8d581499
kitychenjy <51127746+kitychenjy@users.noreply.github.com>
2022-02-26 00:37:30 +0800

    增加了构造数据集与标签的代码

--- FPT_selection.py
+++ FPT_selection.py
@@ -2,42 +2,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-path='data/kurtosis_npy/Bearing1_1kurtosis_v.npy'
-seq_kurtosis=np.load(path)
-plt.plot(seq_kurtosis)
+# path='data/kurtosis_npy/Bearing1_1kurtosis_v.npy'
+# seq_kurtosis=np.load(path)
+# plt.plot(seq_kurtosis)
+#
+# # 使用论文An Improved Exponential Model for
+# # Predicting Remaining Useful Life
+# # of Rolling Element Bearings中的方法筛选
 
-# 使用论文An Improved Exponential Model for
-# Predicting Remaining Useful Life
-# of Rolling Element Bearings中的方法筛选
-all_fpt=[]
-fpt_index=[]
-l=0
-for i , kur in enumerate(seq_kurtosis):
-    fpt=[]
-    if i>1:
-        t_seq=seq_kurtosis[0:i]
-        mean=t_seq.mean()
-        std=t_seq.std()
-        j=i
-        for j in range(l+1):
-            if seq_kurtosis[i+j] > (mean+3*std):
-                ''
-            else:
-                break
-        if j == l:
-            all_fpt.append(i)
-            if len(all_fpt)>1:
-                l=l+1
-                for j in range(l + 1):
-                    if seq_kurtosis[i + j] > (mean + 3 * std):
-                        ''
-                    else:
-                        break
-                if j == l :
-                    print(i)
+def get_fpt(seq_kurtosis):
+    all_fpt = []
+    fpt_index = []
+    l = 0
+    for i, kur in enumerate(seq_kurtosis):
+        fpt = []
+        if i > 1:
+            t_seq = seq_kurtosis[0:i]
+            mean = t_seq.mean()
+            std = t_seq.std()
+            j = i
+            for j in range(l + 1):
+                if seq_kurtosis[i + j] > (mean + 3 * std):
+                    ''
+                else:
                     break
-            else:
-                l = l + 1
+            if j == l:
+                all_fpt.append(i)
+                if len(all_fpt) > 1:
+                    l = l + 1
+                    for j in range(l + 1):
+                        if seq_kurtosis[i + j] > (mean + 3 * std):
+                            ''
+                        else:
+                            break
+                    if j == l:
+                        print(i)
+                        break
+                else:
+                    l = l + 1
+
+
 
 
 
---
+++ make_dataset.py
@@ -0,0 +1,54 @@
+
+# 制作测试西交 和 phm的数据集，并打标签
+#encoding = utf-8
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+from feature_functions import VibrationSignal
+from FPT_selection import get_fpt
+# for XJTU
+con_name= '35Hz12kN'
+path='XJTU-SY_Bearing_Datasets/{}'.format(con_name)
+lable_path = 'lable/{}/'.format(con_name)
+npy_path = 'raw_data/{}/'.format(con_name)
+
+
+
+
+def get_lab(all_data):
+    all_kur=[]
+    length=len(all_data)
+    for data in all_data:
+        b=VibrationSignal(data)
+        kur=b.get_kurtosis()
+        all_kur.append(kur)
+    fpt=get_fpt(all_kur)
+    lable=np.ones(length)
+    life=length-fpt
+    for i in range(fpt,length):
+        lable[i]=(length-i)/life
+    return lable
+
+
+for filename in os.listdir(path):
+    all_data = []
+    all_lab = []
+    file = path + '/' + filename
+    file_list = os.listdir(file)
+    file_list.sort(key=lambda x: int(x[:-4]))  # 读取时发现排序有问题。
+    for f_name in file_list:
+        data_path = file + '/' + f_name
+        data = pd.read_csv(data_path).values
+        all_data.append(data)
+    all_data = np.array(all_data)
+    all_lab = get_lab(all_data)
+    try:
+        np.save(npy_path + filename + '_data.npy', all_data)
+        np.save(lable_path + filename + '_lable.npy', all_lab)
+    except:
+        os.makedirs(npy_path)
+        os.makedirs(lable_path)
+        np.save(npy_path + filename + 'data.npy', all_data)
+        np.save(lable_path + filename + 'lable.npy', all_lab)
